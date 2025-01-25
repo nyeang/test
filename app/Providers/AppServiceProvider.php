d4ea0ce9b0cae0501e55d0a10fc017397d021d8f
nyeang <nyeang@paragoniu.edu.kh>
2025-01-25 21:20:18 +0700

    3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,31 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        // Force HTTPS scheme
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
